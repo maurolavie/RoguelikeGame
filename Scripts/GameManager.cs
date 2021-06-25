b69b4d687025ff64ca00b78aa121c8ad89998a84
Mauro Lavie <19332716+maurolavie@users.noreply.github.com>
2021-06-25 19:08:19 -0300

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using System.Collections.Generic;        //Allows us to use Lists. 
+
+public class GameManager : MonoBehaviour {
+
+	public float levelStartDelay = 2f;
+	public float turnDelay = .1f;
+	public int playerFoodPoints = 100;
+	public static GameManager instance = null; //Static instance of GameManager which allows it to be accessed by any other script.
+	[HideInInspector] public bool playersTurn = true;
+
+	private Text levelText;
+	private GameObject levelImage;
+	private BoardManager boardScript;                        //Store a reference to our BoardManager which will set up the level.
+	private int level = 1;                                    //Current level number, expressed in game as "Day 1".
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool doingSetup;
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
+		if (instance == null)
+		//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+		//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+		Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+
+		enemies = new List<Enemy>();
+
+		//Get a component reference to the attached BoardManager script
+		boardScript = GetComponent<BoardManager>();
+
+		//Call the InitGame function to initialize the first level 
+		InitGame();
+	}
+
+	private void OnLevelWasLoaded(int index)
+	{
+		level++;
+
+		InitGame();
+	}
+
+	// This is calledeach time a scene is loaded.
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+	{
+		//Add one to our level number.
+		level++;
+		//Call InitGame to initialize our level.
+		InitGame();
+	}
+
+	//void OnEnable()
+	//{
+		//Tell our 'OnLevelFinishedLoading' function to start listening for a scene change event as soon as this script is enabled.
+//		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+//	}
+
+//	void OnDisable()
+//	{
+		//Tell our 'OnLevelFinishedLoading' function to stop listening for a scene change event as soon as this script is disabled.
+		//Remember to always have an unsubscription for every delegate you subscribe to!
+//		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+//	}
+
+	//Initializes the game for each level.
+	void InitGame()
+	{
+		doingSetup = true;
+
+		levelImage = GameObject.Find("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text>();
+		levelText.text = "Day " + level;
+		levelImage.SetActive(true);
+		Invoke("HideLevelImage", levelStartDelay);
+
+		enemies.Clear();
+		//Call the SetupScene function of the BoardManager script, pass it current level number.
+		boardScript.SetupScene(level);
+	}
+
+	private void HideLevelImage()
+	{
+		levelImage.SetActive(false);
+		doingSetup = false;
+	}
+
+	public void GameOver()
+	{
+		levelText.text = "After " + level + " days, you starved.";
+		levelImage.SetActive(true);
+		enabled = false;
+	}
+
+	//Update is called every frame.
+	void Update()
+	{
+		if (playersTurn || enemiesMoving || doingSetup)
+			return;
+
+		StartCoroutine(MoveEnemies ());
+	}
+
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add(script);
+	}
+
+	IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;
+		yield return new WaitForSeconds(turnDelay);
+		if (enemies.Count == 0)
+		{
+			yield return new WaitForSeconds(turnDelay);	
+		}
+
+		for (int i = 0; i < enemies.Count; i++) 
+		{
+			enemies[i].MoveEnemy();
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+}
